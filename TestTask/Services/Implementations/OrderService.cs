50ae6dbef3464aeffc714834eadac768f88eb400
OShelest73 <90648269+OShelest73@users.noreply.github.com>
2024-02-12 20:45:21 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public OrderService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Order> GetOrder()
+    {
+        var order = await _dbContext.Orders
+            .OrderByDescending(o => o.Price)
+            .FirstOrDefaultAsync();
+
+        return order;
+    }
+
+    public async Task<List<Order>> GetOrders()
+    {
+        var orders = await _dbContext.Orders.Where(o => o.Quantity > 10).ToListAsync();
+
+        return orders;
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Enums;
+
+namespace TestTask.Services.Implementations;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserService(ApplicationDbContext dbContext)
+    {
+
+        _dbContext = dbContext;
+    }
+
+    public async Task<User> GetUser()
+    {
+        var user = await _dbContext.Users
+            .OrderByDescending(u => u.Orders.Count)
+            .FirstOrDefaultAsync();
+
+        return user;
+    }
+
+    public async Task<List<User>> GetUsers()
+    {
+        var users = await _dbContext.Users.Where(u => u.Status == UserStatus.Inactive).ToListAsync();
+
+        return users;
+    }
+}
